0d4c6a20cd48d0a36109ae6fe265861bdc6718f5
Gabriel Saiago <grsaiago@gmail.com>
2024-06-02 22:25:31 -0300

    Add: Minimal routes <3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+struct ContactRouter {}
+
+impl ContactRouter {
+    pub fn setup_routes(cfg: &mut web::ServiceConfig) {
+        cfg.service(web::scope("/contact"))
+            .route("/", web::get().to(Self::get_all_contacts));
+    }
+
+    async fn get_all_contacts() -> impl Responder {
+        HttpResponse::Ok().body("<h1>Hello World</h1>")
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let _ = HttpServer::new(|| App::new().configure(ContactRouter::setup_routes))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await;
+    Ok(())
 }
